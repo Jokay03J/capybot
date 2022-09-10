7716c0412364ffb01e0e8efcaf9a6a753a60ac7a
jokay03J <55850775+Jokay03J@users.noreply.github.com>
2022-09-10 23:21:12 +0200

    update utils

---
+++ utils/memeList.js
@@ -0,0 +1,23 @@
+const memeList = [
+  "10-Guy",
+  "1990s-First-World-Problems",
+  "1st-World-Canadian-Problems",
+  "2nd-Term-Obama",
+  "Aaaaand-Its-Gone",
+  "Ace-Primo",
+  "Actual-Advice-Mallard",
+  "Art-Student-Owl",
+  "Admiral-Ackbar-Relationship-Expert",
+  "Advice-Dog",
+  "Advice-Doge",
+  "Advice-God",
+  "Advice-Peeta",
+  "Advice-Yoda",
+  "Advice-Tam",
+  "Afraid-To-Ask-Andy",
+  "Afraid-To-Ask-Andy-Closeup",
+  "Aint-Nobody-Got-Time-For-That",
+  "Alan-Greenspan",
+];
+
+module.exports = memeList;
