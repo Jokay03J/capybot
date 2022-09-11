608392bf1aac61653be3e7980c50fd0a36c808c2
jokay03J <55850775+Jokay03J@users.noreply.github.com>
2022-09-11 10:42:28 +0200

    update entry pointe

--- index.js
+++ index.js
@@ -83,9 +83,8 @@ for (const file of commandFiles) {
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 rest
   .put(
-    Routes.applicationGuildCommands(
+    Routes.applicationCommands(
       process.env.CLIENT_ID,
-      process.env.GUILD_ID
     ),
     { body: commands }
   )
